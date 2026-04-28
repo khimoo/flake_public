05419dc21cc7ac2d9433a3a5bcee4cdd225a6879
khimoo <dailysentence1111@gmail.com>
2026-04-28 10:44:20 +0900

    update

--- dotfiles/nvim/lua/plugins/lsp.lua
+++ dotfiles/nvim/lua/plugins/lsp.lua
@@ -1,11 +1,11 @@
 return {
-  {
+  --[[ {
     'cordx56/rustowl',
     version = '*', -- Latest stable version
     build = 'cargo install rustowl',
     lazy = false, -- This plugin is already lazy
     opts = {},
-  },
+  }, ]]
   {
     "j-hui/fidget.nvim",
     opts = {},
