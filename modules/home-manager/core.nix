b34f79f0c0084bb36fdcb096b717ae25a79c5359
khimoo <dailysentence1111@gmail.com>
2026-05-03 12:50:17 +0900

    starshipシンプルにした

--- modules/home-manager/core.nix
+++ modules/home-manager/core.nix
@@ -1,4 +1,11 @@
-{ skk-dict, settings, config, pkgs, lib, ... }:
+{
+  skk-dict,
+  settings,
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 
 {
 
@@ -40,16 +47,13 @@
     starship = {
       enable = true;
       settings = {
-        add_newline = false;
-        command_timeout = 1300;
-        scan_timeout = 50;
-        format = ''
-          $all$nix_shell$nodejs$lua$golang$rust$php$git_branch$git_commit$git_state$git_status
-          $username$hostname$directory'';
+        add_newline = true;
         character = {
-          success_symbol = "[](bold green) ";
-          error_symbol = "[✗](bold red) ";
+          success_symbol = "[❯](bold green)";
+          error_symbol = "[✗](bold red)";
         };
+        package.disabled = true;
+        aws.disabled = true;
       };
     };
     git = {
