db51324394686e18148807b275836021fff401ba
khimoo <dailysentence1111@gmail.com>
2026-06-24 18:24:24 +0900

    display switcherの設定した

--- hosts/nixos-desktop/home-manager-pomu.nix
+++ hosts/nixos-desktop/home-manager-pomu.nix
@@ -15,4 +15,7 @@ in {
 
   dconf.settings."org/gnome/shell".enabled-extensions =
     lib.mkAfter (map (ext: ext.extensionUuid) extraGnomeExtensionsList);
+
+  # スリープ前に active だった display-configuration-switcher 設定を復帰時に再適用する
+  programs.gnomeDisplayDefault.enable = true;
 }
