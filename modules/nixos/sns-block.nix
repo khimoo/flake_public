76aac70ee8919a785fc375bd6078e1056a8beae6
khimoo <dailysentence1111@gmail.com>
2026-09-04 20:08:11 +0900

    flake update

--- modules/nixos/sns-block.nix
+++ modules/nixos/sns-block.nix
@@ -3,12 +3,12 @@
 {
   networking.extraHosts = let
     blockedHosts = [
-      # "x.com"
-      # "www.x.com"
-      # "twitter.com"
-      # "www.twitter.com"
-      # "youtube.com"
-      # "www.youtube.com"
+      "x.com"
+      "www.x.com"
+      "twitter.com"
+      "www.twitter.com"
+      "youtube.com"
+      "www.youtube.com"
     ];
   in builtins.concatStringsSep "\n" (map (host: "0.0.0.0 ${host}") blockedHosts);
 }
